02c22ae3e0620c1bae14c681fef8f8c941934fd4
Joschar22 <chaqquererj@gmail.com>
2023-09-03 23:44:44 -0500

    Mensajes entre vistas

--- resources/views/empleado/edit.blade.php
+++ resources/views/empleado/edit.blade.php
@@ -1,7 +1,8 @@
 formulario de edicfion de empleado
+
 <form action=" {{ url('/empleado/'.$empleado->id) }}" method="post" enctype="multipart/form-data">
 @csrf
 {{ method_field('PATCH') }} 
-@include('empleado.form')
+@include('empleado.form',['modo'=>'Editar'])
     
 </form>
