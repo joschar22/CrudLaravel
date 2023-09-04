dbaf1dc0d18e7a806a7bbc845aea5130e127ab2c
José Chaqquere Rea <joschar22>
2023-09-03 20:44:36 -0500

    validando datos de formulario

--- resources/views/empleado/form.blade.php
+++ resources/views/empleado/form.blade.php
@@ -1,20 +1,21 @@
 
 
 <label for="Nombre">Nombre</label>
-<input type="text" name="Nombre" value="{{ $empleado-> Nombre }}" id="Nombre"><br>
+<input type="text" name="Nombre" value="{{ isset($empleado-> Nombre)?$empleado-> Nombre:'' }}" id="Nombre"><br>
 
 <label for="ApellidoPaterno">Apellido Paterno</label>
-<input type="text" name="ApellidoPaterno" value="{{ $empleado-> ApellidoPaterno }}" id="ApellidoPaterno"><br>
+<input type="text" name="ApellidoPaterno" value="{{ isset($empleado-> ApellidoPaterno)?$empleado-> ApellidoPaterno:'' }}" id="ApellidoPaterno"><br>
 
 <label for="ApellidoMaterno"> Apellido Materno</label>
-<input type="text" name="ApellidoMaterno" value="{{ $empleado-> ApellidoMaterno }}" id="ApellidoMaterno"><br>
+<input type="text" name="ApellidoMaterno" value="{{ isset($empleado-> ApellidoMaterno)?$empleado-> ApellidoMaterno:'' }}" id="ApellidoMaterno"><br>
 
 <label for="Correo">Correo</label>
-<input type="text" name="Correo" value="{{ $empleado-> Correo }}" id="Correo"><br>
+<input type="text" name="Correo" value="{{ isset($empleado-> Correo)?$empleado->Correo:'' }}" id="Correo"><br>
 c
 <label for="Foto ">Foto</label>
-
+@if(isset($empleado->Foto))
 <img src="{{ asset('storage').'/'.$empleado->Foto }}" width="100" alt="">
-<input type="file" name="Foto" value="{{ $empleado-> Foto }}" id="Foto"><br>
+@endif
+<input type="file" name="Foto" value="" id="Foto"><br>
 
 <input type="submit" value="Enviar"><br>
